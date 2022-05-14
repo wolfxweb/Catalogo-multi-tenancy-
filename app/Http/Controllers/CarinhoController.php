30cb87deacd3fc41549fabfd5dd9191c72beb0c9
Eduardo lobo <63437120+wolfxweb@users.noreply.github.com>
2022-05-14 11:13:32 -0300

    Update CarinhoController.php

--- app/Http/Controllers/CarinhoController.php
+++ app/Http/Controllers/CarinhoController.php
@@ -11,62 +11,60 @@ class CarinhoController extends Controller
     {
 
 
-      //  dd($resquest->get('produto'));
+        //  dd($resquest->get('produto'));
         $produto = $resquest->get('produto');
 
-        if(session()->has('cart.'.$produto['id'])){
+        if (session()->has('cart.' . $produto['id'])) {
 
-            $produto  = session()->get('cart.'.$produto['id']);
+            $produto  = session()->get('cart.' . $produto['id']);
 
             $produto['qtd']++;
 
-            session()->forget('cart'.$produto['id']);
+            session()->forget('cart' . $produto['id']);
 
-            session()->put('cart.'.$produto['id'],$produto );
+            session()->put('cart.' . $produto['id'], $produto);
+        } else {
 
-        }else{
-
-           session()->put('cart.'.$produto['id'],$produto);
-
+            session()->put('cart.' . $produto['id'], $produto);
         }
-        return redirect('/')->with('status','Produto adicioando com sucesso');
+        return redirect('/')->with('status', 'Produto adicioando com sucesso');
     }
 
-    public function ajustCart($item , $acao ){
+    public function ajustCart($item, $acao)
+    {
 
-        $msg ='Produto adicioando com sucesso';
+        $msg = 'Produto adicioando com sucesso';
 
         switch ($acao) {
 
             case 'E':
-                  $produto  = session()->get('cart.'.$item);
-                  session()->forget('cart.'.$item);
-                  $this->msg ='Excluido com sucesso';
+                session()->forget('cart.' . $item);
+                $this->msg = 'Excluido com sucesso';
                 break;
 
             case 'A':
-                    $produto  = session()->get('cart.'.$item);
-                    $produto['qtd']++;
-                    session()->forget('cart'.$produto ['id']);
-                    session()->put('cart.'.$produto ['id'],$produto);
-                    $this->msg='Produto adicioando com sucesso';
+
+                $produto  = session()->get('cart.' . $item);
+                $produto['qtd']++;
+                session()->forget('cart' . $produto['id']);
+                session()->put('cart.' . $produto['id'], $produto);
+                $this->msg = 'Produto adicioando com sucesso';
                 break;
 
             case 'D':
-                  /// $this->ajustarQuantidade($item,true);
 
-                    $produto  = session()->get('cart.'.$item);
+                $produto  = session()->get('cart.' . $item);
+
+                if ($produto['qtd'] > 1) {
                     $produto['qtd']--;
-                    if($produto['qtd'] > 1 ){
-                        session()->forget('cart'.$produto ['id']);
-                        session()->put('cart.'.$produto ['id'],$produto);
-                        $this->msg ='Produto diminuido com sucesso';
-                    }else{
-                        $produto  = session()->get('cart.'.$item);
-                        session()->forget('cart.'.$item);
-                        $this->msg ='Excluido com sucesso';
-                    }
-
+                    session()->forget('cart' . $produto['id']);
+                    session()->put('cart.' . $produto['id'], $produto);
+                    $this->msg = 'Produto diminuido com sucesso';
+                } else {
+                    $produto  = session()->get('cart.' . $item);
+                    session()->forget('cart.' . $item);
+                    $this->msg = 'Excluido com sucesso';
+                }
 
                 break;
 
@@ -74,13 +72,6 @@ class CarinhoController extends Controller
                 # code...
                 break;
         }
-      return redirect('/')->with('status',$this->msg);
-    }
-
-    public function ajustarQuantidade($item, $operacao){
-                    $produto  = session()->get('cart.'.$item);
-                    $operacao ? $produto['qtd']-- : $produto['qtd']++;
-                    session()->forget('cart'.$produto ['id']);
-                    session()->put('cart.'.$produto ['id'],$produto);
+        return redirect('/')->with('status', $this->msg);
     }
 }
